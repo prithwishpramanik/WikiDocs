0bdd5cd1e1f0339fbd5caf4e09d86c48313f011a
shuva-shree <baisya14shuvashree@gmail.com>
2025-04-21 23:46:22 +0530

    Initial set up completed for the doctor service

---
+++ DoctorService/src/main/java/com/wikiDocs/DoctorService/controller/DoctorController.java
@@ -0,0 +1,11 @@
+package com.wikiDocs.DoctorService.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctor-service")
+public class DoctorController {
+
+}
---
+++ DoctorService/src/main/java/com/wikiDocs/DoctorService/service/DoctorService.java
@@ -0,0 +1,8 @@
+package com.wikiDocs.DoctorService.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorService {
+}
