87cf40641267d1925000f89dc0e7d2cc23d5f6ca
shuva-shree <baisya14shuvashree@gmail.com>
2025-04-23 00:47:35 +0530

    added createDoctorDetails Post Request

--- DoctorService/src/main/java/com/wikiDocs/DoctorService/controller/DoctorController.java
+++ DoctorService/src/main/java/com/wikiDocs/DoctorService/controller/DoctorController.java
@@ -4,15 +4,14 @@ package com.wikiDocs.DoctorService.controller;
 import com.netflix.discovery.converters.Auto;
 import com.wikiDocs.DoctorService.model.Doctor;
 import com.wikiDocs.DoctorService.service.DoctorService;
+import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -42,6 +41,15 @@ public class DoctorController {
     public ResponseEntity<Doctor> getDoctorById(@PathVariable Integer id){
         try{
             return doctorService.getDoctorById(id);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/createDoctor")
+    public ResponseEntity<Doctor> createDoctorDetails(@RequestPart Doctor doctor,@RequestPart MultipartFile image){
+        try{
+            return doctorService.createDoctorDetails(doctor,image);
         }catch(Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
