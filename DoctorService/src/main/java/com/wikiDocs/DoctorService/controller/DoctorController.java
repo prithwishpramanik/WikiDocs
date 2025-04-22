d3e87e3541aa6365b893f58a13bca7f40e8cb276
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2025-04-22 23:30:42 +0530

    doc change

--- DoctorService/src/main/java/com/wikiDocs/DoctorService/controller/DoctorController.java
+++ DoctorService/src/main/java/com/wikiDocs/DoctorService/controller/DoctorController.java
@@ -5,6 +5,7 @@ import com.netflix.discovery.converters.Auto;
 import com.wikiDocs.DoctorService.model.Doctor;
 import com.wikiDocs.DoctorService.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +23,14 @@ public class DoctorController {
     @Autowired
     DoctorService doctorService;
 
+    @Autowired
+    Environment environment;
+
+
     @GetMapping("/doctors")
     public ResponseEntity<List<Doctor>> getAllDoctors(){
         try{
+            System.out.println(environment.getProperty("local.server.port"));
             return doctorService.getAllDoctors();
         }catch(Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
